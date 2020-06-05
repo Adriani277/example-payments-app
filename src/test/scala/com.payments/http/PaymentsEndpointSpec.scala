77e8dbbc18282380da7d888f684243bf57ace522
Adriani Furtado <adriani.furtado@currencycloud.com>
2020-06-05 12:07:26 +0100

    Fixed failing test

--- src/test/scala/com.payments/http/PaymentsEndpointSpec.scala
+++ src/test/scala/com.payments/http/PaymentsEndpointSpec.scala
@@ -8,11 +8,18 @@ import org.http4s._
 import org.http4s.circe._
 import org.http4s.implicits._
 import com.payments.http.views.PaymentView
+import com.payments.algebras.PaymentCreationAlg
+import com.payments.domain.{Payment, ServiceError}
+import cats.implicits._
 
 final class PaymentsEndpointSpec extends AnyFunSpec with Matchers {
   describe("payment/create endpoint") {
     it("echo's the received payload") {
       implicit val ed = jsonOf[IO, PaymentView]
+
+      val testProgram = new PaymentCreationAlg{
+        def create(payment: Payment): IO[Either[ServiceError,Payment]] = IO.pure(payment.asRight)
+      }
 
       val json = Json.obj(
         ("name", Json.fromString("John Doe")),
@@ -20,7 +27,7 @@ final class PaymentsEndpointSpec extends AnyFunSpec with Matchers {
         ("recipient", Json.fromString("James Doe"))
       )
 
-      val response: IO[Response[IO]] = PaymentsEndpoint.service.run(
+      val response: IO[Response[IO]] = PaymentsEndpoint(testProgram).service.run(
         Request(method = Method.POST, uri = uri"/payment/create")
         .withEntity(json)
       )
